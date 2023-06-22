965cba35175d095d0a6f668126dae9d1e922f6b0
Sandeep Chowdhury <116838065+mrsharifull@users.noreply.github.com>
2023-06-22 13:00:30 +0600

    default registration with assign role --update

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -64,11 +64,13 @@ class RegisterController extends Controller
      */
     protected function create(array $data)
     {
-        return User::create([
+        $user = User::create([
             'name' => $data['name'],
             'email' => $data['email'],
             'password' => Hash::make($data['password']),
             'role_id' => 2,
         ]);
+        $user->assignRole($user->role->name);
+        return $user;
     }
 }
